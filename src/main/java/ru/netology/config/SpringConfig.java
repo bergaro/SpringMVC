74390e643d6b842bee6d790af6ef4d3e05066091
Andrey Izvarin <kriteriykoshi@gmail.com>
2021-07-22 18:58:40 +0300

    Add Spring MVC

---
+++ src/main/java/ru/netology/config/SpringConfig.java
@@ -0,0 +1,19 @@
+package ru.netology.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.json.GsonHttpMessageConverter;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerAdapter;
+import ru.netology.controller.PostController;
+import ru.netology.repository.PostRepository;
+import ru.netology.service.PostService;
+
+@Configuration
+public class SpringConfig {
+    @Bean
+    public RequestMappingHandlerAdapter requestMappingHandlerAdapter() {
+        final var bean = new RequestMappingHandlerAdapter();
+        bean.getMessageConverters().add(new GsonHttpMessageConverter());
+        return bean;
+    }
+}
